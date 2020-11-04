b86e597074b496e713cb78803645de9f1c423c40
shantisa <sns52193@uga.edu>
2020-11-04 17:15:58 -0500

    completed the HTML and CSS for the login and contact php files

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div id="header_style">
 
-    <img src="SAJlogo.png" id="logo" alt="SAJ Logo">
+    <a href="home.php"><img src="SAJlogo.png" id="logo" alt="SAJ Logo"></a>
 
     <div class="search_box">
         <input type="text" placeholder="Search" name="search">
@@ -11,9 +11,9 @@
 
     <div class="nav_menu">
         <ul>
-            <li><a href="construction.html"><b> Products</b></a></li>
+            <li><a href="home.php"><b> Products</b></a></li>
             <li><a href="construction.html"><img src="shopping_cart.svg" alt="Shopping cart"><b>Cart</b></a></li>
-            <li><a href="construction.html"><img src="account_profile.svg" alt="Profile"><b>Login</b></a></li>
+            <li><a href="login.php"><img src="account_profile.svg" alt="Profile"><b>Login</b></a></li>
             <li><a href="construction.html"><b>About</b><img src="dropdown_arrow.svg" alt="Dropdown"></a>
                 <ul>
                     <li><a href="contact.php"><b>Contact Us</b></a></li>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title> SAJ</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="shortcut icon" href="favicon.ico">
+    </head>
+    <body>
+        <!-- Paste the following php files below into your file to obtain the navigation and footer-->
+        <!-- Make sure your files that contains HTML are .php-->
+        <?php include('header.php'); ?>
+
+        <!--Replace the div below with your content-->
+
+        <div id="login">
+
+            <form name="login_form">
+                <fieldset id="login_section">
+                    <legend><b>Login/Create Account</b></legend>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" required><br>
+                    <label for="passwrd">Password:</label>
+                    <input type="text" id="passwrd" required><br>
+                    <input type="submit" value="Login" id="login_submit"> <br>
+                    <a href="construction.html" id="contact_link">Are you a new user?</a>
+                </fieldset>
+            </form>
+
+        </div>
+
+        <?php include('footer.php'); ?>
+    </body>
+</html>
