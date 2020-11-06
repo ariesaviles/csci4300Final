910136dfb47bc2b210c59860c6fd3efa7671c85c
jct13761 <jct13761@uga.edu>
2020-11-06 18:28:03 -0500

    Added newUser and PasswordMatch files

---
+++ passwordMatch.js
@@ -0,0 +1,39 @@
+newFunction();
+
+function newFunction() {
+    "use strict";
+    var $ = function(id) {
+        return document.getElementById(id);
+    };
+
+    var joinList = function() {
+
+        var errorMessage = "";
+        var password = $("passwrd").value;
+        var confirmPassword = $("confirmPasswrd").value;
+
+        console.log(password);
+        console.log(confirmPassword);
+
+        // validate the entries
+        if (password != confirmPassword) {
+            errorMessage = "Passwords do not match";
+        } else {
+
+        }
+        // && password.length >= 6 && confirmPassword.legnth >= 6
+
+        // submit the form if all entries are valid
+        // otherwise, display an error message
+        if (errorMessage == "") {
+            $("email_form").submit();
+        } else {
+            alert(errorMessage);
+        }
+    };
+
+    window.onload = function() {
+        $("newUser_submit").onclick = joinList;
+        $("username").focus();
+    };
+}
